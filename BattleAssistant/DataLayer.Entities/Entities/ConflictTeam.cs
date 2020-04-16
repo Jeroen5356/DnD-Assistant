3b9b27e03d4f96f7d55100efbd2c21903790d56b
Jeroen <j_v_d_k@hotmail.com>
2020-04-16 08:25:27 +0200

    Added Color to db, not to code yet. Removed navbar. css: src with or without double dots, makes a difference for some reason
    see link for conversion: https://stackoverflow.com/questions/2276144/storing-and-retrieving-colors-with-database-c-sharp-windows-forms-application

--- BattleAssistant/DataLayer.Entities/Entities/ConflictTeam.cs
+++ BattleAssistant/DataLayer.Entities/Entities/ConflictTeam.cs
@@ -8,6 +8,7 @@ namespace DataLayer.Entities.Entites
     {
         public Guid ConflictId { get; set; }
         public Guid TeamId { get; set; }
+        public string Color { get; set; }
         public virtual Conflict Conflict { get; set; }
         public virtual Team Team { get; set; }
     }
---
+++ BattleAssistant/DataLayer.Migrations/Migrations/20200416061721_Add_Color_To_ConflictTeam.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations.Migrations
+{
+    public partial class Add_Color_To_ConflictTeam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "ConflictTeam",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "ConflictTeam");
+        }
+    }
+}
