bc16cd15664afba9cfc181ddc4e36deb8ec2e097
Jeroen <j_v_d_k@hotmail.com>
2020-04-19 11:36:49 +0200

    Creating SuperUser works now, only problem is, you can't log in for some reason. Added Constants

---
+++ BattleAssistant/BattleAssistant/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleAssistant
+{
+    public static class Constants
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string SuperAdminEmail = "SuperAdmin@mail.com";
+    }
+}
